fa222976dc894f1228c7b607520f4e8cd84b9a5d
Takahito Nakano <kano@ponko2.jp>
2026-04-18 00:43:46 +0900

    chore(deps): update Rust to 1.95.0

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
             packages = with pkgs; [
               (pkgs.fenix.fromToolchainFile {
                 file = ./rust-toolchain.toml;
-                sha256 = "qqF33vNuAdU5vua96VKVIwuc43j4EFeEXbjQ6+l4mO4=";
+                sha256 = "gh/xTkxKHL4eiRXzWv8KP7vfjSk61Iq48x47BEDFgfk=";
               })
               cargo-features-manager
               cargo-machete
