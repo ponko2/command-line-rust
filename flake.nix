b96e4cbdb474de340de44dd0152973fb8e3c6240
Takahito Nakano <kano@ponko2.jp>
2026-08-23 19:32:36 +0900

    chore: migrate to hk

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
               commitlint
               deadnix
               editorconfig-checker
-              lefthook
+              hk
               nixd
               nixfmt
               oxfmt
